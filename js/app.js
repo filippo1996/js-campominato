b09cdaea5f388cb59e7a8a5911cb2f419fa26f5c
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-06-08 16:43:04 +0200

    add algoritmo generazione numeri e inserimento nell'array

--- js/app.js
+++ js/app.js
@@ -8,3 +8,51 @@
  * Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
  * BONUS: (da fare solo se funziona tutto il resto) all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali: con difficoltà 0 => tra 1 e 100 con difficoltà 1 => tra 1 e 80 con difficoltà 2 => tra 1 e 50
  */
+
+//Livello difficoltà
+var difficulty = 100;
+
+//Creiamo l'array contenente i 16 numeri generati da 1 a difficulty (default 100)
+console.log(getCreateNumber(difficulty));
+
+/**
+ * Funzione che cerca se l'elemento esiste nell'array
+ * @param {array} array 
+ * @param {[string,number]} serch 
+ * @returns {boolean}
+ */
+function searchElement(array, serch){
+    for(var i = 0; i < array.length; i++){
+        if(array[i] === serch){
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * Crazione di numeri generati
+ * @returns {array}
+ */
+function getCreateNumber(rangeMax){
+    //Array contenente i 16 numeri casuali
+    var itemsNum = [];
+    while(itemsNum.length < 16){
+        //Funzione che genera casualmente i numeri
+        var randomNum = getRndInteger(1, rangeMax);
+        if(!searchElement(itemsNum, randomNum)){
+            itemsNum.push(randomNum);
+        }
+    }
+    return itemsNum;
+}
+
+/**
+ * Generatore di numeri casuali
+ * @param {number} min 
+ * @param {number} max 
+ * @returns {number}
+ */
+function getRndInteger(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
